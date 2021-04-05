ad95220052fb07d0561dd1a884c9c0322b85bcc5
saleem51 <orinoco@mail.fr>
2021-04-05 19:01:05 +0200

    project finished

--- class/Message.php
+++ class/Message.php
@@ -6,13 +6,21 @@ class Message
     const LIMIT_MESSAGE  = 10;
     private $username;
     private $message;
+    private $date ;
   
+
+    public static function fromJSON(string $json): Message
+    {
+        $data = json_decode($json, true);
+        return new self($data['username'], $data['message'], new DateTime("@" . $data['date']));
+    }
 
 
     public function __construct(string $username, string $message, ?DateTime $date = null )
     {
         $this->username = $username;
         $this->message = $message;
+        $this->date = $date ?: new DateTime();
     }
 
     public function isValid():bool
@@ -31,6 +39,30 @@ class Message
             $errors['message'] = 'Votre message est trop court';
         }
         return $errors;
+    }
+
+    public function toHTML(): string
+    {
+        $username = htmlentities($this->username);
+        $this->date->setTimezone(new DateTimeZone('Europe/Paris'));
+        $date = $this->date->format('d/m/Y à H:i');
+        $message = nl2br(htmlentities($this->message));
+        return <<<HTML
+        <p>
+            <strong>{$username}</strong> <em>{$date}</em><br>
+            {$message}
+        </p>
+HTML;
+    }
+
+    public function toJSON(): string
+    {
+       return json_encode([
+            'username' => $this->username,
+            'message' => $this->message,
+            'date' =>  $this->date->getTimestamp()
+        ]);
+
     }
 
 }
--- index.php
+++ index.php
@@ -1,9 +1,11 @@
 <?php 
 
 require_once './class/Message.php'; 
+require_once './class/GuestBook.php';
 
-
+$success = false;
 $errors = null;
+$guestbook = new GuestBook(__DIR__ . DIRECTORY_SEPARATOR . 'data' . DIRECTORY_SEPARATOR . 'messages');
 if(isset($_POST['username'], $_POST['message'])){
 
 $message = new Message($_POST['username'], $_POST['message']);
@@ -11,7 +13,9 @@ $message = new Message($_POST['username'], $_POST['message']);
 
 if($message->isValid()) {
 
-
+$guestbook->addMessage($message);
+$success = true;
+$_POST = [];
 
 } else {
     $errors = $message->getErrors();
@@ -19,6 +23,7 @@ if($message->isValid()) {
 
 }
 
+$messages = $guestbook->getMessages();
 $title = "livre d'or" ; ?>
 
 <?php require './element/header.php' ?>
@@ -32,15 +37,23 @@ $title = "livre d'or" ; ?>
     </div>
     <?php endif ?>
 
+
+    <?php if($success) : ?>
+    <div class="alert alert-success">
+        Merci pour votre message
+    </div>
+    <?php endif ?>
+
+
     <form action="" method="post">
         <div class="form-group mt-4 mb-4">
-            <input value="<?= htmlentities($_POST['username']) ?? '' ?>" type="text" name="username" placeholder="Votre pseudo" class="form-control <?= isset($errors['username']) ? 'is-invalid' : '' ?>">
+            <input value="<?= $_POST['username'] ?? '' ?>" type="text" name="username" placeholder="Votre pseudo" class="form-control <?= isset($errors['username']) ? 'is-invalid' : '' ?>">
             <?php if(isset($errors['username'])): ?>
             <div class="invalid-feedback"><?= $errors['username'] ?></div>
             <?php endif ?>
         </div>
         <div class="form-group mt-2 mb-4">
-            <textarea  name="message" placeholder="Votre message" class="form-control <?= isset($errors['message']) ? 'is-invalid' : '' ?>"><?= htmlentities($_POST['message']) ?? '' ?></textarea>
+            <textarea  name="message" placeholder="Votre message" class="form-control <?= isset($errors['message']) ? 'is-invalid' : '' ?>"><?= $_POST['message'] ?? '' ?></textarea>
             <?php if(isset($errors['message'])): ?>
             <div class="invalid-feedback"><?=$errors['message'] ?></div>
             <?php endif ?>
@@ -48,8 +61,14 @@ $title = "livre d'or" ; ?>
         <button class="btn btn-primary">Envoyer</button>
     </form>
 
+    <?php if (!empty ($messages)): ?>
+    <h2 class="mt-4 mb-4">Vos messages</h2>
 
+    <?php foreach($messages as $message): ?>
+        <?= $message->toHTML() ?>
+    <?php endforeach ?>
 
+    <?php endif ?>
 </div>
 
 
